98cc6865ee7ec8de678eec9f2a4eb75713bb4c75
abdellatif2001 <72755668+abdellatif2001@users.noreply.github.com>
2021-09-17 18:15:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,6 @@ import pandas as pd
 import streamlit as st
 import pickle 
 
-
-slider_ph = st.empty()
-info_ph = st.empty()
-
-value = slider_ph.slider("slider", 0, 100, 25, 1)
-info_ph.info(value)
-
-if st.button('animate'):
-    for x in range(20):
-        time.sleep(.5)
-
-        value = slider_ph.slider("slider", 0, 100, value + 1, 1)
-        info_ph.info(value)
-
 st.title('More wine More happiness')
 st.write('## quality of your wine')
 st.write('### enter the infos')
